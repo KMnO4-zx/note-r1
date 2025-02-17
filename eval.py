a440ebd3af845f84182a0e8ab1f7176614b83d30
KMnO4-zx <1021385881@qq.com>
2025-02-17 12:28:19 +0800

    更新 eval.py，隐藏 API 密钥

--- eval.py
+++ eval.py
@@ -4,7 +4,7 @@ from evalscope.config import TaskConfig
 task_cfg = TaskConfig(
     model='deepseek-r1',
     api_url='https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions',
-    api_key='sk-26dbb562231740ef92f004ed10a41ab5',
+    api_key='xxx',
     eval_type='service',
     datasets=['iquiz'],
     generation_config={
